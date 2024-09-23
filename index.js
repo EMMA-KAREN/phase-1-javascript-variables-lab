b1d3edd072db5942579e4ec5a5f3275988e2154c
EMMA KAREN <mumokaren26@gmail.com>
2024-09-24 01:48:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood ="Chelsea";
+
+let companyCeo = "Susan Smith";
+
+// if (companyCeo.toLowerCase() === "susan smith".toLowerCase()) {
+//     console.log("The CEO is Susan Smith.");
+// } else {
+//     console.log("The CEO is not Susan Smith.");
+// }
